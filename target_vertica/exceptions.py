d5ea05953a943e7bbda5ca7980b0179b60b1304d
Jordan Ryan <jordan@facetinteractive.com>
2021-05-17 11:42:07 +0530

    Remove unnecessary "pass" statement
    (cherry picked from commit fdd301f6532328a292d47e74a83b97981c66f2e7)

--- target_vertica/exceptions.py
+++ target_vertica/exceptions.py
@@ -4,9 +4,7 @@ from vertica_python.errors import CopyRejected
 
 class RecordValidationException(Exception):
     """Exception to raise when record validation failed"""
-    pass
 
 
 class InvalidValidationOperationException(Exception):
     """Exception to raise when internal JSON schema validation process failed"""
-    pass
